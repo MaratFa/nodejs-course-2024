430dab4ecd69ea9ac6d32dc0d57f246512f71651
MaratFa <evolution99@rambler.ru>
2024-11-16 00:41:03 +0300

    Edited

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import http from "http";
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  // res.setHeader("Content-Type", "text/html");
-  // res.statusCode = 404;
-
-  res.writeHead(500, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "Server Error" }));
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>Homepage</h1>");
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>About</h1>");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<h1>Not Found</h1>");
+  }
 });
 
 server.listen(PORT, () => {
